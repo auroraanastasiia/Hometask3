e562db010722384dc988d71b0f6c57caf881822c
aurora <auroraflesh15@gmail.com>
2023-09-29 20:55:34 +0300

    home task 2

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
 try:
- print("Hello this program about days of the week!")
- n1 = int(input("Give me numbers from 1 to 7 :"))
- if n1 == 0:
-    print("Please enter non-zero number!")
- elif n1 > 7:
-    print("This number is not for program")
- elif n1 == 1:
-    print("Monday")
- elif n1 == 2:
-    print("Tuesday")
- elif n1 == 3:
-    print("Wednesday")
- elif n1 == 4:
-    print("Thursday")
- elif n1 == 5:
-    print("Friday")
- elif n1 == 6:
-    print("Saturday")
- elif n1 == 7:
-    print("Sunday")
-
-except ValueError as error:
-    print("Enter only integer numbers please!")
-
-finally: print("End of the program")
+ print("This program about two numbers ")
+ c1 = int(input("Give me the first number :"))
+ c2 = int(input("Give me the second number : "))
+ if c1 == c2:
+    print(c1, "equals ", c2)
+ elif c1 > c2:
+    print(c1, "bigger", c2)
+ elif c1 < c2:
+    print(c2, "bigger", c1)
+finally:
+    print("End of program ")
